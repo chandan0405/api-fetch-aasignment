f9551726272a33dd8efd6fd87837e3fc4666adf3
Chandan.Kumar <chandan.kumar@intelliswift.com>
2024-10-24 12:42:54 +0530

    project setup

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to the reactjs</h1>
+      </div>
+      
+    </>
+  )
+}
